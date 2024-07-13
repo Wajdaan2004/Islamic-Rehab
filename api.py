d19acc1369cd565fe606b235c54833b2d0c04cc3
Wajdaan <1wajdaanmalik@gmail.com>
2024-07-13 15:32:11 -0400

    api ting

--- api.py
+++ api.py
@@ -1,3 +1,18 @@
 from flask import Flask, request, jsonify
-def func():
-    return 100
+
+app = Flask(__name__)
+
+@app.route("/get-hadith/hadith")
+def get_hadith():
+    hadiths ={
+        "1": "one",
+        "2": "two",
+        "3": "three",
+        "4": "four",
+        "5": "five",
+    }
+    return jsonify(hadiths),200
+
+
+if "__name__" == "__api__":
+    app.run(debug=True)
